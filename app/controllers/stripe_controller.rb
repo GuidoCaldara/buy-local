78cb273813ecc46e95a05fd0365c6592aab5f036
guido caldara <guido.caldara@gmail.com>
2020-04-01 15:46:49 +0800

    ok

--- app/controllers/stripe_controller.rb
+++ app/controllers/stripe_controller.rb
@@ -6,19 +6,18 @@ class StripeController < ApplicationController
         code: params[:code],
         grant_type: "authorization_code"
       }
-    )
+      )
 
     if response.parsed_response.key?("error")
       redirect_to user_settings_path(user),
-        notice: response.parsed_response["error_description"]
+      notice: response.parsed_response["error_description"]
     else
       stripe_user_id = response.parsed_response["stripe_user_id"]
       current_merchant.update_attribute(:stripe_user_id, stripe_user_id)
       redirect_to store_dashboard_path,
-        notice: 'User successfully connected with Stripe!'
+      notice: 'User successfully connected with Stripe!'
     end
 
   end
 
-  end
 end
